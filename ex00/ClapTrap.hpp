d07210912269dc97fa57fccbf786f0be0f27acc3
taka-chin <tyamauch@c3r1s1.42tokyo.jp>
2024-07-05 20:01:38 +0900

    toriaezu

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -18,6 +18,8 @@ public:
   void attack(const std::string &target);
   void takeDamage(unsigned int amount);
   void beRepaired(unsigned int amount);
+  const std::string getName();
+	const int getAttackDamage();
 };
 
 #endif
---
+++ ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap test("田中");
+	test.attack("山田");
+}
