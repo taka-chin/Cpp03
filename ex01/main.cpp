05aaa32235c0e5dd9159641ab324501c07dcf17c
taka-chin <tyamauch@c3r3s3.42tokyo.jp>
2024-07-08 22:09:55 +0900

    iroiro

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,26 @@
 #include "ScavTrap.hpp"
+#include <iostream>
 
 int main() {
-  ScavTrap player("田中");
-  ScavTrap player1("山田");
+  ScavTrap tanaka("田中");
+  ScavTrap yamada("山田");
+  int i = 0;
 
-  player.attack("山田");
-  player1.takeDamage(20);
-  player1.beRepaired(4294967285);
-  player.guardGate();
-
-  player1.attack("田中");
-  player.takeDamage(20);
-  player.beRepaired(4294967286);
-  player1.guardGate();
+  while (tanaka.getHitPoints() && yamada.getHitPoints()) {
+    std::cout << "Turn: " << i++ << std::endl;
+	if(tanaka.getHitPoints())
+	{
+    	tanaka.attack("山田");
+    	yamada.takeDamage(20);
+    	yamada.beRepaired(1);
+    	yamada.guardGate();
+	}
+	if(yamada.getHitPoints())
+	{
+    	yamada.attack("田中");
+    	tanaka.takeDamage(20);
+    	tanaka.beRepaired(1);
+    	tanaka.guardGate();
+	}
+  }
 }
